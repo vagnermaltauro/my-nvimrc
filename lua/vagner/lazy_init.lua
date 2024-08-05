9e9b2a6b0af87b0fa66fa0a933a8929ad3ccbe5d
vagnermaltauro <vagnermaltauro@gmail.com>
2024-08-05 12:01:26 -0300

    feat: update  lazy initialization configuration for rocks

--- lua/vagner/lazy_init.lua
+++ lua/vagner/lazy_init.lua
@@ -12,6 +12,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "vagner.lazy",
-    change_detection = { notify = false }
+  rocks = {
+    enabled = false,
+  },
+  spec = "vagner.lazy",
+  change_detection = { notify = false }
 })
